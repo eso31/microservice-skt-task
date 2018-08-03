d664e25b6b712f3b948deae501041f04e29978e8
Dilan Coss <dilanarma@hotmail.com>
2018-08-02 20:39:22 -0600

    Adds a logger, changes rabbitmq port

--- src/main/java/com/skytask/listener/Listener.java
+++ src/main/java/com/skytask/listener/Listener.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skytask.channel.ProductSource;
 import com.skytask.model.Product;
 import com.skytask.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -18,16 +20,18 @@ public class Listener {
     @Autowired
     private ProductService productService;
 
+    private static Logger logger = LoggerFactory.getLogger(Listener.class);
+
     @StreamListener("productsChannelOutput")
-    public void gettingProduct(Product product) {
-        System.out.println("I received: " + product);
+    public void createProduct(Product product) {
+        logger.info("I received: " + product);
         productService.create(product);
     }
 
     @StreamListener("productListChannelOutput")
     @SendTo("productListChannelInput")
-    public String gettingRequest(String message) throws JsonProcessingException {
-        System.out.println("I received " + message);
+    public String getProductList(String message) throws JsonProcessingException {
+        logger.info("I received " + message);
         List<Product> products = productService.getList();
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(products);
